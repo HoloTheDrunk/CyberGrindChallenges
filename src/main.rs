0545b2a2d6f7b33c99af398ee995321a48f0369a
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2023-07-22 17:53:03 +0200

    feat: add actix webserver and logging middleware

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,33 @@
 mod db;
 mod models;
 
-// use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-// #[actix_web::main]
-// async fn main() -> std::io::Result<()> {
-//     pretty_env_logger::init();
-//
-//     HttpServer::new(move || {
-//         let cors = actix_cors::Cors::permissive();
-//
-//         App::new().wrap(cors)
-//     })
-//     .bind(("localhost", 8080))?
-//     .run()
-//     .await
-// }
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
 
 use models::{scores::Score, variations::Variation};
+use serde_json::json;
 use sqlx::{postgres::PgPoolOptions, FromRow, Pool, Postgres};
 
-#[tokio::main]
-async fn main() {
+#[get("/")]
+async fn status() -> impl Responder {
+    HttpResponse::Ok().json(json!({"status": "online"}))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     dotenv::dotenv().expect("Unable to load environment variables from .env file");
+    pretty_env_logger::init();
 
-    let mut variation = Variation::default();
-    variation.use_piercer();
+    HttpServer::new(move || {
+        let cors = actix_cors::Cors::permissive();
 
-    dbg!(variation);
+        App::new()
+            .service(status)
+            .configure(models::scores::init_routes)
+            .configure(models::variations::init_routes)
+            .wrap(cors)
+            .wrap(Logger::default())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
